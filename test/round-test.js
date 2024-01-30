e4dd454f1a20952865c97ee7453411d4226aef6d
Asher Spurr <asher.spurr@gmail.com>
2024-01-30 16:34:51 -0700

    pass 3rd takeTurn test

--- test/round-test.js
+++ test/round-test.js
@@ -100,7 +100,15 @@ describe('takeTurn', () => {
 
         expect(takeTurn('object', round)).to.equal('correct!')
     });
-    it.skip('should return feedback if false', () => {
+    it('should return feedback if false', () => {
+        const card1 = createCard(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+        const card2 = createCard(2, "What is a comma-separated list of related values?", ["array", "object", "function"], "array");
+        const card3 = createCard(3, "What type of prototype method directly modifies the existing array?", ["mutator method", "accessor method", "iteration method"], "mutator method");
+        
+        const deck = createDeck([card1, card2, card3]);
+        const round = createRound(deck);
+
+        expect(takeTurn('fruit', round)).to.equal('incorrect!')
 
     });
     it.skip('should update incorrectGuesses property if false', () => {
